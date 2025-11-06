2ff7f22667a499be639470c98617fd09c2948f8a
김혜인 <rjqnrdl7524@naver.com>
2025-11-06 11:47:17 +0900

    Create Project

---
+++ Assets/Scripts/Defines.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public static class DataTableIds
+{
+    public static readonly string String = "StringTable";
+}
+
+public static class Tags
+{
+    // public static readonly string GameController = "GameController";
+}
